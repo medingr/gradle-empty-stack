ea32334c7db85ca9e5524764535cdcc64f917682
GRAY JEANNE MEDINA (SHP-IRIS-ISD-OOCLL/MNL) <MEDINGR2@OOCL.COM>
2019-10-10 13:45:44 +0800

    finish story 2

--- src/main/java/Android.java
+++ src/main/java/Android.java
@@ -1,4 +1,7 @@
 public class Android extends Mobile {
+    String name="AndriodName";
+    String color="blue";
+    String brand="Android";
     @Override
     public void call(String message) {
         System.out.println("<Android>Message: " + message);
--- src/main/java/Mobile.java
+++ src/main/java/Mobile.java
@@ -1,7 +1,32 @@
 public class Mobile {
+
     private String name ="";
     private String color="";
     private String brand="";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 
     public static void main(String[] args) {
         Mobile myPhone = new Mobile();
@@ -10,9 +35,19 @@ public class Mobile {
         myPhone.call("This is a message");
         apple.call("Apple Message");
         and.call("Android Message");
+
+        //show specs
+        myPhone.setBrand("Android");
+        myPhone.setColor("Red");
+        myPhone.setName("Gray");
+        myPhone.showSpecs();
+
     }
     public void call(String message) {
         System.out.println("Message: " + message);
     };
 
+    public void showSpecs() {
+        System.out.println("Name: " + name + ", color: " + color + ", brand: " + brand );
+    };
 }
